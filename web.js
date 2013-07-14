703f7b975633b472855731cc7602cb8afb87ae74
Ubuntu <ubuntu@ip-172-31-44-226.us-west-2.compute.internal>
2013-07-14 17:39:25 +0000

    MODIF DE WE.JS

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var buffer = new Buffer(16);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+ fs.readSync('index.html', buffer);
+ response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
